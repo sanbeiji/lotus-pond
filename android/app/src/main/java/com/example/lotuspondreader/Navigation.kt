ccf6f2b29f8e581133862a5eb30a152ab9afd6b4
Joseph R. Lewis <30529+sanbeiji@users.noreply.github.com>
2026-05-20 10:03:45 -0700

    Fix build: add compose foundation scroll extension imports

--- android/app/src/main/java/com/example/lotuspondreader/Navigation.kt
+++ android/app/src/main/java/com/example/lotuspondreader/Navigation.kt
@@ -12,6 +12,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
